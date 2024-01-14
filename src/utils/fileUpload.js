b06f90861c4b21a1becfb42beab12078b98153b5
Vivek Bhatt <vivekbhatt618@gmail.com>
2024-01-14 15:30:19 +0530

    user controller fixed

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -13,7 +13,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("File uploaded successfully", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
